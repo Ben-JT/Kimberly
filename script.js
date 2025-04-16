3c9b744345ae6e735a9812430e2acdaab27d06d6
Jhosis <20200850@une.edu.pe>
2025-04-16 18:43:27 -0500

    actial

--- script.js
+++ script.js
@@ -21,3 +21,12 @@ noBtn.addEventListener("mouseover", () => {
   noBtn.style.left = randomX + "px";
   noBtn.style.top = randomY + "px";
 });
+
+
+document.addEventListener('click', () => {
+    const audio = document.getElementById('myAudio');
+    if (audio.muted) {
+      audio.muted = false; // Desactiva el silencio
+    audio.play();
+    }
+});
